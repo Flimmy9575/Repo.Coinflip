18b98fbf13a67e069d1dce2a4ceb21c4335d18a8
Dante Hasty <dante9562@gmail.com>
2025-06-17 17:00:34 -0400

    feat: Add LocationValidator for gambling location validation
    Implements `LocationValidator` to determine if gambling is allowed in the current player location. Includes checks for shop-only gambling mode and logs validation results.

---
+++ Coinflip/Validators/LocationValidator.cs
@@ -0,0 +1,20 @@
+﻿namespace CoinFlip.Validators;
+
+public static class LocationValidator
+{
+    public static bool CanGambleInCurrentLocation()
+    {
+        var shopOnlyGambling = Coinflip.ShopOnly;
+        
+        if (shopOnlyGambling && !SemiFunc.RunIsShop())
+        {
+            Coinflip.Logger.LogInfo("[CoinFlip] Player is not in the shop and shop only gaming is enabled. Not continuing.");
+            return false;
+        }
+        
+        Coinflip.Logger.LogInfo("[CoinFlip] Player is in the shop. Continuing...");
+        
+        return true;
+    }
+
+}
